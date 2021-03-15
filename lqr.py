8dea4b1c0c0039d00d59aea637bb9cbafbb845ca
Marc Sabaté <marcsv87@gmail.com>
2021-03-15 17:15:51 +0000

    Update lqr.py

--- lqr.py
+++ lqr.py
@@ -19,7 +19,7 @@ class CoefsLQR:
         self.M = torch.eye(d).to(device)
         self.C = torch.zeros(d, d).to(device)
         self.D = torch.zeros(d, d).to(device)
-        self.F = torch.zeros(d, d).to(device)
+        self.F = torch.eye(d).to(device)
         self.R = torch.eye(d).to(device)
         self.sigma = 1
 
